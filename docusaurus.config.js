2599fc1f929da2254b30da43402dbfbfaede633d
Victor Gouin <victor.gouin1@icloud.com>
2024-06-18 15:45:57 +0200

    documentations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,6 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -55,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Victor Gouin',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -65,7 +63,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentations',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
