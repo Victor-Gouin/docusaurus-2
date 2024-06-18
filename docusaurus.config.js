f0166a735d0f673425e6e928bb8631f6fc47c835
Victor Gouin <victor.gouin1@icloud.com>
2024-06-18 16:05:15 +0200

    intro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             items: [
               {
                 label: 'Documentations',
-                to: '/docs',
+                to: '/docs/intro',
               },
             ],
           },
